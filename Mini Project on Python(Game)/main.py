9bca2b1f4a7e5c5945ce1772b2d66056ed64db4f
Sidratul Muntaha Oyshe <oyshe241-15-886@diu.edu.bd>
2025-05-12 19:07:30 +0600

    Updated version.py
    This is the updated version

--- Mini Project on Python(Game)/main.py
+++ Mini Project on Python(Game)/main.py
@@ -1,27 +1,31 @@
 import random
-computer = random.choice([-1,0,1])
-youstr=input("Enter Your Choice: ")
-youDict={"s":1 , "w":-1 ,"g":0}
-you=youDict[youstr] #choice will convert to number
-# Now we have 2 numbers,computer and you
-reverseDict={1:"Snake",-1:"Water",0:"Gun"} 
-#to convert numbers into word
-print(f"You Choose {reverseDict[you]}\nComputer Choose {reverseDict[computer]}")
-if(computer==you):
-    print("It's a draw")
-else:
-    if(computer==1 and you==-1):
-        print("Computer Wins")
-    elif(computer==-1 and you==1):
-        print("You Win")
-    elif(computer==1 and you==0):
-        print("You Win")
-    elif(computer==0 and you==1):
-        print("Computer Wins")
-    elif(computer==-1 and you==0):
-        print("Computer Wins")
-    elif(computer==0 and you==-1):
-        print("You Win")
+try:
+    computer = random.choice([-1,0,1])
+    youstr=input("Enter Your Choice: ")
+    youDict={"s":1 , "w":-1 ,"g":0}
+    you=youDict[youstr] #choice will convert to number
+    # Now we have 2 numbers,computer and you
+    reverseDict={1:"Snake",-1:"Water",0:"Gun"} 
+    #to convert numbers into word
+    print(f"You Choose {reverseDict[you]}\nComputer Choose {reverseDict[computer]}")
+    if(computer==you):
+        print("It's a draw")
     else:
-        print("Something Went Wrong!")
-        
+        if(computer==1 and you==-1):
+            print("Computer Wins")
+        elif(computer==-1 and you==1):
+            print("You Win")
+        elif(computer==1 and you==0):
+            print("You Win")
+        elif(computer==0 and you==1):
+            print("Computer Wins")
+        elif(computer==-1 and you==0):
+            print("Computer Wins")
+        elif(computer==0 and you==-1):
+            print("You Win")
+
+    
+except Exception as e:
+      print("Something Went Wrong! Make sure to enter 's' or 'w' or 'g' not",e)        
+
+
